ebe99ab7fe6351f3f41522b4e3f4db8a50a95469
Gavin Swofford <gavinswofford@gmail.com>
2020-10-07 14:47:43 -0500

    started adding category controller, repo interface and model

---
+++ Tabloid/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace TabloidMVC.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Error { get; set; }
+
+    }
+}
---
+++ Tabloid/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ICategoryRepository
+    {
+        List<Category> GetAll();
+        void AddCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(int categoryId);
+        Category GetCategoryById(int categoryId);
+    }
+}
