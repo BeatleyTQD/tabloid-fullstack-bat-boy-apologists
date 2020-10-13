26ee7cd94c9969cac0202c877525cb78d8a8b4ef
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-10-13 14:09:20 -0500

    typo fix

--- Tabloid/client/src/components/Category/CategoryEditForm.js
+++ Tabloid/client/src/components/Category/CategoryEditForm.js
@@ -41,13 +41,13 @@ const CategoryEditForm = () => {
         <>
             <div className="container pt-5">
                 <div className="card col-md12">
-                    <h3 className="mt-3 text-primary text-center card-title">Add a Category</h3>
+                    <h3 className="mt-3 text-primary text-center card-title">Edit a Category</h3>
                     <form className="mt-5 card-body">
                         <div className="form-group">
                             <input className="form-control" id="name" value={category.name} onChange={handleFieldChange} />
                         </div>
                         <div className="form-group">
-                            <input type="button" value="Add" className="btn btn-primary btn-block" onClick={SaveCategory} />
+                            <input type="button" value="Save" className="btn btn-primary btn-block" onClick={SaveCategory} />
                             <input type="button" value="Cancel" className="btn btn-primary btn-block" onClick={Cancel} />
                         </div>
                     </form>
